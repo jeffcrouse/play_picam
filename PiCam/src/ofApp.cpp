e3e395577533978876b9f8bb58d50d247aee4fa0
Jeff Crouse <jefftimesten@gmail.com>
2014-10-13 16:46:59 -0400

    PiCam

---
+++ PiCam/src/ofApp.cpp
@@ -0,0 +1,106 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofSetLogLevel(OF_LOG_VERBOSE);
+	ofSetLogLevel("ofThread", OF_LOG_SILENT);
+	ofSetVerticalSync(false);
+	ofEnableAlphaBlending();
+
+
+	consoleListener.setup(this);
+	
+	omxCameraSettings.width = 1280;
+	omxCameraSettings.height = 720;
+	omxCameraSettings.framerate = 30;
+	omxCameraSettings.isUsingTexture = true;
+
+	videoGrabber.setup(omxCameraSettings);
+	filterCollection.setup();
+
+	cout << "listening for osc messages on port " << PORT << "\n";
+	receiver.setup(PORT);
+
+	font.loadFont("verdana.ttf", 48);
+	current_msg_string = 0;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	// check for waiting messages
+	while(receiver.hasWaitingMessages()){
+		// get the next message
+		ofxOscMessage m;
+		receiver.getNextMessage(&m);
+        
+		// check for mouse moved message
+		if(m.getAddress() == "/message"){
+			current_msg_string = m.getArgAsString();
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	ofSetColor(225);
+	videoGrabber.draw();
+
+	verdana14.drawString(current_msg_string, 30, 35);
+}
+
+//--------------------------------------------------------------
+void shaderApp::onCharacterReceived(SSHKeyListenerEventData& e)
+{
+	keyPressed((int)e.character);
+}
+
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	ofLogVerbose(__func__) << key;
+	
+	if (key == 'e')
+	{
+		videoGrabber.applyImageFilter(filterCollection.getNextFilter());
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ PiCam/src/ofApp.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+#include "ConsoleListener.h"
+#include "ofxRPiCameraVideoGrabber.h"
+
+#define PORT 12345
+
+class ofApp : public ofBaseApp, public SSHKeyListener{
+	public:
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		ofTrueTypeFont font;
+		ofxOscReceiver receiver;
+
+		int current_msg_string;
+
+		void onCharacterReceived(SSHKeyListenerEventData& e);
+		ConsoleListener consoleListener;
+		ofxRPiCameraVideoGrabber videoGrabber;
+
+		OMXCameraSettings omxCameraSettings;
+		ImageFilterCollection filterCollection;
+};
